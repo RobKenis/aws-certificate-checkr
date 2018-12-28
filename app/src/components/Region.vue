d126f016a9ce2444c1f210581237f3dbfeb8cf9c
Rob Kenis <rob.kenis@vrt.be>
2018-12-28 17:09:30 +0100

    Add buttons per region
    TODO: Dynamic load regions

---
+++ app/src/components/Region.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+        <b-button v-on:click="loadRegion" class="region-button" variant="primary">{{name}}</b-button>
+    </div>
+</template>
+
+<script>
+    import bButton from 'bootstrap-vue/es/components/button/button'
+
+    export default {
+        name: 'Region',
+        props: {
+            name: String,
+        },
+        components: {
+            'b-button': bButton
+        },
+        methods: {
+            loadRegion: function () {
+                console.log("Requesting info for [" + this.name + "]");
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    @import url('https://fonts.googleapis.com/css?family=Roboto+Mono');
+
+    .region-button {
+        font-family: 'Roboto', sans-serif;
+        margin: 1em;
+        float: left;
+    }
+</style>
---
+++ app/src/components/Regions.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="regions">
+        <h1>Regions</h1>
+        <div class="regions-list">
+            <Region v-for="region in regions" v-bind:key="region" :name="region"></Region>
+        </div>
+    </div>
+</template>
+
+<script>
+    import Region from './Region.vue'
+
+    export default {
+        name: 'Regions',
+        data: function () {
+            return {regions: ["eu-central-1", "eu-west-1", "us-east-1"]}
+        },
+        components: {
+            Region
+        }
+    }
+</script>
+
+<style scoped>
+    .regions-list {
+        display: inline;
+    }
+</style>
