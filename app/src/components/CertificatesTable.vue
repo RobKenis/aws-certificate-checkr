6d4206c94b9c203845f1934949c6681e5f69026e
Rob Kenis <rob.kenis@vrt.be>
2018-12-31 16:03:59 +0100

    Add search filter on certificates table

--- app/src/components/CertificatesTable.vue
+++ app/src/components/CertificatesTable.vue
@@ -1,18 +1,51 @@
 <template>
-  <BTable
-    hover
-    :items="certificates"
-    :fields="fields"
-  />
+  <div>
+    <BFormGroup
+      horizontal
+      label="Filter"
+      class="mb-0"
+    >
+      <BInputGroup>
+        <BFormInput
+          v-model="filter"
+          placeholder="Type to Search"
+        />
+        <BInputGroupAppend>
+          <BButton
+            :disabled="!filter"
+            @click="filter = ''"
+          >
+            Clear
+          </BButton>
+        </BInputGroupAppend>
+      </BInputGroup>
+    </BFormGroup>
+    <BTable
+      hover
+      :items="certificates"
+      :fields="fields"
+      :filter="filter"
+    />
+  </div>
 </template>
 
 <script>
-    import bTable from 'bootstrap-vue/es/components/table/table'
+  import bTable from 'bootstrap-vue/es/components/table/table';
+  import bFormGroup from 'bootstrap-vue/es/components/form/form';
+  import bFormInput from 'bootstrap-vue/es/components/form-input/form-input';
+  import bInputGroup from 'bootstrap-vue/es/components/input-group/input-group';
+  import bInputGroupAppend from 'bootstrap-vue/es/components/input-group/input-group-append';
+  import bButton from 'bootstrap-vue/es/components/button/button';
 
-    export default {
+  export default {
         name: 'CertificatesTable',
         components: {
-            'BTable': bTable
+            'BTable': bTable,
+            'BFormGroup': bFormGroup,
+            'BInputGroup': bInputGroup,
+            'BFormInput': bFormInput,
+            'BInputGroupAppend': bInputGroupAppend,
+            'BButton': bButton
         },
         props: {
             certificates: {
@@ -24,6 +57,7 @@
         },
         data: function () {
             return {
+                filter: null,
                 fields: [
                     {key: 'CertificateArn', label: 'Certificate ARN', sortable: true},
                     {key: 'DomainName', label: 'Domain Name', sortable: true},
